1bd19ac59a7fcb4211ca8946b1ba70b058bc0d5f
masara <ooua814@gmail.com>
2025-03-15 11:04:31 +0700

    publish to github

--- prac1/tongbangso.cpp
+++ prac1/tongbangso.cpp
@@ -1,3 +1,29 @@
+// #include <bits/stdc++.h>
+// typedef unsigned long long ull;
+// typedef long long ll;
+// using namespace std;
+// #define v vector
+// char el = '\n';
+
+// int main() {
+//   ll m, n;
+//   ll s = 1532023;
+//   cin >> m >> n;
+//   ll a[m][n];
+//   for (ll i = 0; i < m; i++) {
+//     for (ll j = 0; j < n; j++) {
+//       if ((i + j) % 2 != 0) {
+//         a[i][j] = ((((i - 1) % s) * (n % s)) % s + (j % s)) % s;
+//       } else {
+//         a[i][j] = 0;
+//       }
+//       cout << a[i][j] << " ";
+//     }
+//     cout << el;
+//   }
+//   return 0;
+// }
+
 #include <bits/stdc++.h>
 typedef unsigned long long ull;
 typedef long long ll;
@@ -6,18 +32,20 @@ using namespace std;
 char el = '\n';
 
 int main() {
-  int m, n;
+  ll m, n;
+  ll s = 1532023;
   cin >> m >> n;
-  int a[m][n];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if ((i + j) % 2 == 1) {
-        a[i][j] = 0;
+  ll a[m][n];
+  for (ll i = 0; i < m; i++) {
+    for (ll j = 0; j < n; j++) {
+      if ((i + j) % 2 != 0) {
+        a[i][j] = (((((i - 1) % s + s) % s) * (n % s)) % s + (j % s)) % s;
       } else {
-        a[i][j] = (i - 1) * n + j;
+        a[i][j] = 0;
       }
       cout << a[i][j] << " ";
     }
     cout << el;
   }
+  return 0;
 }
